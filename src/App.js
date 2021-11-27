2b1d9f9441d860d1e895d9f92137a9bb972c27fe
Kevin Weaver <kevinweaver2@gmail.com>
2021-11-27 10:25:03 -0500

    Add particles.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Plexus from "./Plexus.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Plexus />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
